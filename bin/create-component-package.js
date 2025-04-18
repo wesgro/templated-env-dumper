e22805055ea97c720b1af91cac3bae3e7ba8a163
Jacob Ketcheson <snowcave@gmail.com>
2025-04-17 21:26:41 -0700

    Refactor create-component-package.js to improve modularity by extracting createPrompter and execAction functions. This enhances readability and maintainability of the code.

--- bin/create-component-package.js
+++ bin/create-component-package.js
@@ -1,18 +1,50 @@
 #!/usr/bin/env node
+// @ts-check
 
 const { runner } = require('hygen')
 const { default: Logger } = require('hygen/dist/logger')
 const path = require('path')
+
 const defaultTemplates = path.join(__dirname, '..', '_templates')
+
+/**
+ * @typedef {import('enquirer')} Enquirer
+ * @typedef {import('execa').ExecaReturnValue} ExecaReturnValue
+ */
+
+/**
+ * @template T
+ * @typedef {Object} Prompter
+ * @property {(questions: any) => Promise<T>} prompt
+ */
+
+/**
+ * @template Q,T
+ * @returns {Prompter<T>}
+ */
+const createPrompter = () => {
+  const enquirer = require('enquirer')
+  return {
+    prompt: questions => enquirer.prompt(questions)
+  }
+}
+
+/**
+ * @param {string} action
+ * @param {string | undefined} body
+ * @returns {Promise<ExecaReturnValue>}
+ */
+const execAction = (action, body) => {
+  const execa = require('execa')
+  const opts = body && body.length > 0 ? { input: body } : {}
+  return execa(action, { shell: true, ...opts })
+}
 
 runner(['package', 'new'], {
   templates: defaultTemplates,
   cwd: process.cwd(),
   logger: new Logger(console.log.bind(console)),
-  createPrompter: () => require('enquirer'),
-  exec: (action, body) => {
-    const opts = body && body.length > 0 ? { input: body } : {}
-    return require('execa').shell(action, opts)
-  },
+  createPrompter,
+  exec: execAction,
   debug: !!process.env.DEBUG
 }) 
